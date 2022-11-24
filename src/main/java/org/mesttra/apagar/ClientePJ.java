ce43be777921c7b8545b67d4432c14e5ee830cc4
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-11-23 21:55:11 -0300

    Included List and ClienteDAO

---
+++ src/main/java/org/mesttra/apagar/ClientePJ.java
@@ -0,0 +1,7 @@
+package org.mesttra.apagar;
+
+public class ClientePJ extends Cliente {
+    public ClientePJ(String cnpj, String[] nomeSocio, String nomeSocial, String nomeFantasia, String telefone, double limiteChequeEspecial) {
+        super();
+    }
+}
